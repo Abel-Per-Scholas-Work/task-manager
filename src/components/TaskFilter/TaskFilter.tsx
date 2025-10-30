7972cf94363221395ed0450933a4b6c94ee42110
belunatic <abelluvyou@yahoo.com>
2025-10-30 15:26:02 -0700

    I can filter the tasks for status and priority

--- src/components/TaskFilter/TaskFilter.tsx
+++ src/components/TaskFilter/TaskFilter.tsx
@@ -1,23 +1,43 @@
 import type { TaskFilterProps } from "../../types";
 import React, { useState } from "react";
 
-export default function TaskFilter() {
+export default function TaskFilter({ onFilterChange }: TaskFilterProps) {
 	const [filter, setFilter] = useState({
-		status: "all-statuses",
-		priority: "all-priorities",
+		status: undefined,
+		priority: undefined,
 	});
+
+	const handleStatusChanges = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const { name, value } = e.target;
+		const newValue = value === "" ? undefined : value;
+		//create a new object
+		const newFilter = { ...filter, [name]: newValue };
+		//immediate update the state by passing a new object
+		setFilter(newFilter);
+
+		console.log(newFilter);
+		onFilterChange(newFilter);
+
+		/**
+		 * delays the update of the state.
+		 */
+		// setFilter((prev) => ({ ...prev, [name]: value }));
+
+		// console.log(filter);
+		// onFilterChange(filter);
+	};
+
 	return (
 		<div className="text-black flex p-4 gap-x-4">
 			<div className=" flex flex-col gap-y-2 w-auto ">
 				<label htmlFor="status">Status</label>
 				<select
 					id="optionsSelect"
-					value={filter.status}
-					onChange={(e) => console.log(e.target.value)}
+					value={filter?.status}
+					name="status"
+					onChange={handleStatusChanges}
 					className="bg-gray-800 text-white p-2 rounded-lg">
-					<option
-						value="all-statuses"
-						selected={filter.status === "all-statuses"}>
+					<option value="" selected={!filter.status}>
 						All Statuses
 					</option>
 					<option value="pending" selected={filter.status === "pending"}>
@@ -37,21 +57,20 @@ export default function TaskFilter() {
 				<label htmlFor="status">Priority</label>
 				<select
 					id="optionsSelect"
-					value={filter.status}
-					onChange={(e) => console.log(e.target.value)}
+					value={filter?.priority}
+					name="priority"
+					onChange={handleStatusChanges}
 					className="bg-gray-800 text-white p-2 rounded-lg">
-					<option
-						value="all-priorities"
-						selected={filter.status === "all-priorities"}>
+					<option value="" selected={!filter.priority}>
 						All Priorities
 					</option>
-					<option value="low" selected={filter.status === "low"}>
+					<option value="low" selected={filter.priority === "low"}>
 						Low
 					</option>
-					<option value="medium" selected={filter.status === "medium"}>
+					<option value="medium" selected={filter.priority === "medium"}>
 						Medium
 					</option>
-					<option value="high" selected={filter.status === "high"}>
+					<option value="high" selected={filter.priority === "high"}>
 						High
 					</option>
 				</select>
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,24 +1,52 @@
-import type { TaskListProps, Task } from "../../types";
+import { useState } from "react";
+import type { Task, TaskListProps, TaskStatus } from "../../types";
 import TaskFilter from "../TaskFilter/TaskFilter";
 import TaskItem from "../TaskItem/TaskItem";
-import { useState } from "react";
+
+interface Filter {
+	status?: TaskStatus;
+	priority?: "low" | "medium" | "high";
+}
 
 export default function TaskList({
 	tasks,
 	onStatusChange,
 	onDelete,
 }: TaskListProps) {
-	//viewed list
-	const [viewTasks, setViewTasks] = useState<Task[]>(tasks);
+	const [taskList, setTaskList] = useState(tasks);
+
+	const onFilterChange = (filters: Filter) => {
+		console.log("these are the ", filters);
+		let result = [];
+		if (!filters.priority && !filters.status) {
+			result = tasks;
+			console.log("I am in here");
+		} else {
+			result = tasks.filter((task) => {
+				if (filters.status && filters.priority) {
+					return (
+						task.status === filters.status && task.priority == filters.priority
+					);
+				}
+				if (filters.status) {
+					return task.status === filters.status;
+				}
+				if (filters.priority) {
+					return task.priority === filters.priority;
+				}
+			});
+		}
+		setTaskList(result);
+	};
 
 	return (
 		<div>
 			<div>
-				<TaskFilter />
+				<TaskFilter onFilterChange={onFilterChange} />
 			</div>
 
 			<div className="flex flex-col gap-y-4 ">
-				{viewTasks.map((item) => {
+				{taskList.map((item) => {
 					return (
 						<TaskItem
 							key={item.id}
